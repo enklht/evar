0874109abd273965b26beadb49bf702a1aae9103
enklht <enklht@gmail.com>
2025-03-16 14:40:05 +1300

    feat: :sparkles: add functions

--- src/eval.rs
+++ src/eval.rs
@@ -8,7 +8,7 @@ fn factorial(n: f64) -> f64 {
 
     match result {
         Some(f) => f as f64,
-        None => f64::MAX,
+        None => f64::INFINITY,
     }
 }
 
@@ -39,6 +39,42 @@ pub fn eval(expr: Expr, context: &Context) -> f64 {
             match (function, &context.angle_unit) {
                 (Sin, Radian) => eval(*arg, context).sin(),
                 (Sin, Degree) => eval(*arg, context).to_radians().sin(),
+                (Cos, Radian) => eval(*arg, context).cos(),
+                (Cos, Degree) => eval(*arg, context).to_radians().cos(),
+                (Tan, Radian) => eval(*arg, context).tan(),
+                (Tan, Degree) => eval(*arg, context).to_radians().tan(),
+                (Sec, Radian) => 1.0 / eval(*arg, context).cos(),
+                (Sec, Degree) => 1.0 / eval(*arg, context).to_radians().cos(),
+                (Csc, Radian) => 1.0 / eval(*arg, context).sin(),
+                (Csc, Degree) => 1.0 / eval(*arg, context).to_radians().sin(),
+                (Cot, Radian) => 1.0 / eval(*arg, context).tan(),
+                (Cot, Degree) => 1.0 / eval(*arg, context).to_radians().tan(),
+                (Asin, Radian) => eval(*arg, context).asin(),
+                (Asin, Degree) => eval(*arg, context).asin().to_degrees(),
+                (Acos, Radian) => eval(*arg, context).acos(),
+                (Acos, Degree) => eval(*arg, context).acos().to_degrees(),
+                (Atan, Radian) => eval(*arg, context).atan(),
+                (Atan, Degree) => eval(*arg, context).atan().to_degrees(),
+                (Asec, Radian) => eval(*arg, context).recip().acos(),
+                (Asec, Degree) => eval(*arg, context).recip().acos().to_degrees(),
+                (Acsc, Radian) => eval(*arg, context).recip().asin(),
+                (Acsc, Degree) => eval(*arg, context).recip().asin().to_degrees(),
+                (Acot, Radian) => eval(*arg, context).recip().atan(),
+                (Acot, Degree) => eval(*arg, context).recip().atan().to_degrees(),
+                (Sinh, _) => eval(*arg, context).sinh(),
+                (Cosh, _) => eval(*arg, context).cosh(),
+                (Tanh, _) => eval(*arg, context).tanh(),
+                (Floor, _) => eval(*arg, context).floor(),
+                (Ceil, _) => eval(*arg, context).ceil(),
+                (Round, _) => eval(*arg, context).round(),
+                (Abs, _) => eval(*arg, context).abs(),
+                (Sqrt, _) => eval(*arg, context).sqrt(),
+                (Exp, _) => eval(*arg, context).exp(),
+                (Exp2, _) => eval(*arg, context).exp2(),
+                (Ln, _) => eval(*arg, context).ln(),
+                (Log10, _) => eval(*arg, context).log10(),
+                (Rad, _) => eval(*arg, context).to_radians(),
+                (Deg, _) => eval(*arg, context).to_degrees(),
             }
         }
         Expr::BinaryFunctionCall {
@@ -49,6 +85,7 @@ pub fn eval(expr: Expr, context: &Context) -> f64 {
             use BinaryFunction::*;
             match (function, &context.angle_unit) {
                 (Log, _) => eval(*arg1, context).log(eval(*arg2, context)),
+                (NRoot, _) => eval(*arg1, context).powf(eval(*arg2, context).recip()),
             }
         }
     }
@@ -274,5 +311,121 @@ mod tests {
             rhs: Box::new(Expr::Number(1.0)),
         };
         assert_eq!(eval(expr, &RADIAN_CONTEXT), 4.0);
+    }
+    #[test]
+    fn test_eval_cos_radian() {
+        let expr = Expr::UnaryFunctionCall {
+            function: UnaryFunction::Cos,
+            arg: Box::new(Expr::Number(0.0)),
+        };
+        assert_eq!(eval(expr, &RADIAN_CONTEXT), 1.0);
+    }
+
+    #[test]
+    fn test_eval_cos_degree() {
+        let expr = Expr::UnaryFunctionCall {
+            function: UnaryFunction::Cos,
+            arg: Box::new(Expr::Number(0.0)),
+        };
+        assert_eq!(eval(expr, &DEGREE_CONTEXT), 1.0);
+    }
+
+    #[test]
+    fn test_eval_tan_radian() {
+        let expr = Expr::UnaryFunctionCall {
+            function: UnaryFunction::Tan,
+            arg: Box::new(Expr::Number(std::f64::consts::PI / 4.0)),
+        };
+        assert_eq!(eval(expr, &RADIAN_CONTEXT), 1.0);
+    }
+
+    #[test]
+    fn test_eval_tan_degree() {
+        let expr = Expr::UnaryFunctionCall {
+            function: UnaryFunction::Tan,
+            arg: Box::new(Expr::Number(45.0)),
+        };
+        assert_eq!(eval(expr, &DEGREE_CONTEXT), 1.0);
+    }
+
+    #[test]
+    fn test_eval_sqrt() {
+        let expr = Expr::UnaryFunctionCall {
+            function: UnaryFunction::Sqrt,
+            arg: Box::new(Expr::Number(16.0)),
+        };
+        assert_eq!(eval(expr, &RADIAN_CONTEXT), 4.0);
+    }
+
+    #[test]
+    fn test_eval_exp() {
+        let expr = Expr::UnaryFunctionCall {
+            function: UnaryFunction::Exp,
+            arg: Box::new(Expr::Number(1.0)),
+        };
+        assert_eq!(eval(expr, &RADIAN_CONTEXT), std::f64::consts::E);
+    }
+
+    #[test]
+    fn test_eval_ln() {
+        let expr = Expr::UnaryFunctionCall {
+            function: UnaryFunction::Ln,
+            arg: Box::new(Expr::Number(std::f64::consts::E)),
+        };
+        assert_eq!(eval(expr, &RADIAN_CONTEXT), 1.0);
+    }
+
+    #[test]
+    fn test_eval_log10() {
+        let expr = Expr::UnaryFunctionCall {
+            function: UnaryFunction::Log10,
+            arg: Box::new(Expr::Number(1000.0)),
+        };
+        assert_eq!(eval(expr, &RADIAN_CONTEXT), 3.0);
+    }
+
+    #[test]
+    fn test_eval_exp2() {
+        let expr = Expr::UnaryFunctionCall {
+            function: UnaryFunction::Exp2,
+            arg: Box::new(Expr::Number(3.0)),
+        };
+        assert_eq!(eval(expr, &RADIAN_CONTEXT), 8.0);
+    }
+
+    #[test]
+    fn test_eval_floor() {
+        let expr = Expr::UnaryFunctionCall {
+            function: UnaryFunction::Floor,
+            arg: Box::new(Expr::Number(3.7)),
+        };
+        assert_eq!(eval(expr, &RADIAN_CONTEXT), 3.0);
+    }
+
+    #[test]
+    fn test_eval_ceil() {
+        let expr = Expr::UnaryFunctionCall {
+            function: UnaryFunction::Ceil,
+            arg: Box::new(Expr::Number(3.3)),
+        };
+        assert_eq!(eval(expr, &RADIAN_CONTEXT), 4.0);
+    }
+
+    #[test]
+    fn test_eval_round() {
+        let expr = Expr::UnaryFunctionCall {
+            function: UnaryFunction::Round,
+            arg: Box::new(Expr::Number(3.5)),
+        };
+        assert_eq!(eval(expr, &RADIAN_CONTEXT), 4.0);
+    }
+
+    #[test]
+    fn test_eval_abs() {
+        let expr = Expr::UnaryFunctionCall {
+            function: UnaryFunction::Abs,
+            arg: Box::new(Expr::Number(-3.5)),
+        };
+        assert_eq!(eval(expr, &RADIAN_CONTEXT), 3.5);
     }
 }
