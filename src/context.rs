8d805a36dd889b71d11d195188e99e60e08cb3b4
enklht <enklht@gmail.com>
2025-03-18 21:56:57 +1300

    feat: :sparkles: implement unary function evaluations

--- src/context.rs
+++ src/context.rs
@@ -6,15 +6,55 @@ pub struct Context {
     functions: HashMap<String, Function>,
 }
 
-impl Context {
-    pub fn new(args: Args) -> Context {
-        let mut functions = [(
-            "sin".into(),
+macro_rules! unary_fn {
+    ($fname:ident) => {
+        (
+            stringify!($fname).into(),
             Function::External {
                 arity: 1,
-                body: |x| x.get(0).unwrap().sin(),
+                body: |x| x.first().unwrap().$fname(),
             },
-        )]
+        )
+    };
+    ($fname:ident, $body:expr) => {
+        (
+            stringify!($fname).into(),
+            Function::External {
+                arity: 1,
+                body: $body,
+            },
+        )
+    };
+}
+
+impl Context {
+    pub fn new(args: Args) -> Context {
+        let functions = [
+            unary_fn!(sin),
+            unary_fn!(cos),
+            unary_fn!(tan),
+            unary_fn!(sec, |x| x.first().unwrap().sin().recip()),
+            unary_fn!(csc, |x| x.first().unwrap().cos().recip()),
+            unary_fn!(cot, |x| x.first().unwrap().tan().recip()),
+            unary_fn!(asin),
+            unary_fn!(acos),
+            unary_fn!(atan),
+            unary_fn!(sinh),
+            unary_fn!(cosh),
+            unary_fn!(tanh),
+            unary_fn!(floor),
+            unary_fn!(ceil),
+            unary_fn!(round),
+            unary_fn!(abs),
+            unary_fn!(sqrt),
+            unary_fn!(exp),
+            unary_fn!(exp2),
+            unary_fn!(ln),
+            unary_fn!(log2),
+            unary_fn!(log10),
+            unary_fn!(rad, |x| x.first().unwrap().to_radians()),
+            unary_fn!(deg, |x| x.first().unwrap().to_degrees()),
+        ]
         .into();
         Context { functions }
     }
