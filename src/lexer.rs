0aa18fd9cf79d1bba02986d7b4fb568e5618096b
enklht <enklht@gmail.com>
2025-03-21 23:53:06 +1300

    fix: :bug: update regex patterns for integer and float tokens to improve parsing accuracy

--- src/lexer.rs
+++ src/lexer.rs
@@ -7,10 +7,10 @@ pub enum Token<'a> {
     #[regex(r"\s+")]
     Space,
 
-    #[regex(r"-?(0|[1-9]\d*)", |lex| lex.slice().parse::<i32>().unwrap())]
+    #[regex(r"\d*", |lex| lex.slice().parse::<i32>().unwrap())]
     Int(i32),
 
-    #[regex(r"-?(0|[1-9]\d*)((\.\d+([eE][+-]?\d+)?)|((\.\d+)?[eE][+-]?\d+))", |lex| lex.slice().parse::<f64>().unwrap())]
+    #[regex(r"(?:\d+\.?\d*|\.\d+)(?:[eE][-+]?\d+)?", |lex| lex.slice().parse::<f64>().unwrap())]
     Float(f64),
 
     #[token("let")]
